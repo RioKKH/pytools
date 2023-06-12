e06ccc9298789b04ddf1c8ef71ad3a8a2df19de2
Ryoichi <ryoichikakehi@gmail.com>
2023-06-13 01:01:55 +0900

    modified:   perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import traceback
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,55 +10,103 @@ import matplotlib.pyplot as plt
 
 class Perceptron():
 
-    def __init__(self, w):
+    def __init__(self):
         # training data
         self.X = np.array([[1, 1.2],  [1, 0.2],  [1, -0.2],
                            [1, -0.5], [1, -1.0], [1, -1.5]])
         # class label
         self.t = np.array([1, 1, 1, -1, -1, -1])
         # Initialize weight coefficients
-        self.w = np.array(w)
-        #self.w = np.array([0.5, 0.5])
-        #self.w = np.array([0.5, 0.5]).reshape([-1, 1])
+        self.wini = None
+        self.w = None
+        self.w0 = []
+        self.w1 = []
 
         # Learning coefficients
         self.roh = 0.5 # 学習係数ρ
+        self.num_of_loop = 1
+
+
+    def init(self):
+        self.__init__()
+
 
     def descriminant_function(self, x):
         return np.where(x >= 0, 1, -1)
 
-    def evaluate(self, w, x, t):
+
+    def evaluate(self, w, x, t, fw):
         y = self.descriminant_function(np.sum(w * x))
         # 識別関数と正解が異なれば重みを更新
         print(f"%.2f,%.2f,%.2f,%.2f,%.2f,%d,%d"
               %(x[0], x[1], w[0], w[1], np.sum(w * x), t, y))
-
+        fw.write(f"%.2f,%.2f,%.2f,%.2f,%.2f,%d,%d\n"
+                 %(x[0], x[1], w[0], w[1], np.sum(w * x), t, y))
         if (t != y):
-            #print("before:", t, w, x)
-            #print(self.roh * t * x)
-            w += self.roh * t * x
-            #print("after:", t, w, x)
+            dw = self.roh * t * x
+            w = w + dw
+
+        self.w0.append(w[0])
+        self.w1.append(w[1])
         return w
+
 
     def train(self):
         # training with simple perceptron
-        num_of_loop = 1
+        self.num_of_loop = 1
+        fw = open(f"weight_%.2f_%.2f.log" % (self.wini[0], self.wini[1]), "w")
+
         while 1:
-            print("\n", num_of_loop, "巡目", "w=", self.w)
+            print("\n", self.num_of_loop, "巡目", "w=", self.w)
+            fw.write("\n%d巡目 w=[%.2f, %.2f]\n"
+                     % (self.num_of_loop, self.w[0], self.w[1]))
+
             for i in range(self.t.size):
-                self.w = self.evaluate(self.w, self.X[i], self.t[i])
+                self.w = self.evaluate(self.w, self.X[i], self.t[i], fw)
 
             y = self.descriminant_function(np.sum(self.w * self.X, 1))
-            num_of_loop += 1
+            self.num_of_loop += 1
 
             if all(y == self.t):
-                print("\n", num_of_loop, "巡目", "w=", self.w)
+                print("\n", self.num_of_loop, "巡目", "w=", self.w)
+                fw.write("\n%d巡目 w=[%.2f, %.2f]\n"
+                         % (self.num_of_loop, self.w[0], self.w[1]))
+
                 for i in range(self.t.size):
-                    w = self.evaluate(self.w, self.X[i], self.t[i])
+                    w = self.evaluate(self.w, self.X[i], self.t[i], fw)
                 break
 
-    def predict(self):
-        pass
+        fw.close()
+
+
+    def set_weight(self, w0, w1):
+        self.wini = np.array([w0, w1])
+        self.w = np.array([w0, w1])
+        self.w0.append(w0)
+        self.w1.append(w1)
+
+
+    def save(self, fout='result.csv'):
+        with open(fout, 'a') as fw:
+            fw.write("%.2f,%.2f,%d\n" 
+                     % (self.wini[0], self.wini[1], self.num_of_loop))
+
+
+    def plot(self):
+        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(5, 5))
+        ax.plot(self.w0, self.w1,
+                marker='o', ms=2, color='black', alpha=0.5, ls='-')
+        ax.scatter(self.w0[0], self.w1[0], s=40, color='red')
+        ax.scatter(self.w0[-1], self.w1[-1], s=40, color='green')
+        ax.set_xlim(-3, 3)
+        ax.set_ylim(-3, 3)
+        ax.grid(ls='dashed', color='gray', alpha=0.5)
+        plt.title(f"Initial weight (w0, w1) = (%.2f, %.2f)"
+                  % (self.wini[0], self.wini[1]))
+        plt.savefig(f"weight_%.2f_%.2f.png" % (self.wini[0], self.wini[1]))
+        plt.cla()
+        plt.clf()
+
 
 def parse_double(value):
     try:
@@ -67,6 +116,9 @@ def parse_double(value):
 
 
 if __name__ == '__main__':
+    eps = 1E-6
+    X = np.arange(-2, 2+eps, 0.25)
+    Y = np.arange(-2, 2+eps, 0.25)
     parser = argparse.ArgumentParser(
         description = "Simple perceptron"
     )
@@ -77,6 +129,17 @@ if __name__ == '__main__':
     )
     args = parser.parse_args()
 
-    perceptron = Perceptron(args.w)
-    perceptron.train()
+    try:
+        perceptron = Perceptron()
+        for x in X:
+            for y in Y:
+                perceptron.init()
+                perceptron.set_weight(x, y)
+                perceptron.train()
+                perceptron.plot()
+                perceptron.save()
+
+    except Exception as e:
+        print(e)
+        print(trackback.format_exc())
 
