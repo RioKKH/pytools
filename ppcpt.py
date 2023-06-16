55477434f7dc777df111cc9ad33411a65f147e57
Ryoichi <ryoichikakehi@gmail.com>
2023-06-17 01:22:14 +0900

    modified:   ppcpt.py

--- ppcpt.py
+++ ppcpt.py
@@ -19,11 +19,13 @@ def plot(df:pd.DataFrame) -> None:
     plt.yticks(np.arange(0, 110, 10))
     plt.show()
 
-def cmp_hist2(df1, df2) -> None:
+def cmp_hist2(df1, df2, name1="ref", name2="tgt") -> None:
     fig, ax0 = plt.subplots(nrows=1, ncols=1)
     bins = range(df1['num'].min(), df1['num'].max() +2)
-    df1['num'].plot.hist(bins=bins, align='left', ax=ax0, alpha=0.5, label='original')
-    df2['num'].plot.hist(bins=bins, align='left', ax=ax0, alpha=0.5, label='myModel')
+    df1['num'].plot.hist(
+        bins=bins, align='left', ax=ax0, alpha=0.5, label=name1, ec='black')
+    df2['num'].plot.hist(
+        bins=bins, align='left', ax=ax0, alpha=0.5, label=name2, ec='black')
     ax0.grid(ls='dashed', color='gray', alpha=0.3)
     ax0.set_xticks(np.arange(0, 20))
     ax0.set_yticks(np.arange(0, 110, 10))
@@ -42,10 +44,22 @@ def cmp_hist3(df1, df2, df3) -> None:
     plt.legend()
     plt.show()
 
-def heatmap(df1, df2) -> None:
+def heatmap(df1, df2, name1="ref", name2="tgt") -> None:
     fig, [ax0, ax1] = plt.subplots(nrows=1, ncols=2, figsize=(9, 6))
-    reg = df1.pivot_table(columns="wo", index="w1", values="num")[::-1]
-    mym = df2.pivot_table(columns="wo", index="w1", values="num")[::-1]
-    sns.heatmap(reg, square=True, ax=ax0)
-    sns.heatmap(mym, square=True, ax=ax1)
+    reg = df1.pivot_table(columns="w0", index="w1", values="num")[::-1]
+    mym = df2.pivot_table(columns="w0", index="w1", values="num")[::-1]
+    #sns.heatmap(reg, square=True)
+    sns.heatmap(reg, square=True, ax=ax0,
+                cbar=True, cbar_kws={"shrink":0.5}, cmap='coolwarm',
+                annot=True, vmin=0, vmax=15)
+    sns.heatmap(mym, square=True, ax=ax1,
+                cbar=True, cbar_kws={"shrink":0.5}, cmap='coolwarm',
+                annot=True, vmin=0, vmax=15)
+    #sns.heatmap(reg, square=True, ax=ax0, vmin=0, vmax=15)
+    #sns.heatmap(mym, square=True, ax=ax1, vmin=0, vmax=15)
+    #sns.heatmap(reg, square=True, ax=ax0, cbar_ax=ax1, vmin=0, vmax=15)
+    #sns.heatmap(mym, square=True, ax=ax1, cbar_ax=ax1, vmin=0, vmax=15)
+    ax0.set_title(name1)
+    ax1.set_title(name2)
+    plt.tight_layout()
     plt.show()
