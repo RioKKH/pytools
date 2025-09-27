ac413d6a4222d28e1953f0e7e78ed60b1f90daef
Ryoichi <ryoichikakehi@gmail.com>
2025-09-27 10:35:29 +0900

    modified:   DeepLearning04/ch04/policy_iter.py

--- DeepLearning04/ch04/policy_iter.py
+++ DeepLearning04/ch04/policy_iter.py
@@ -13,7 +13,7 @@ from ch04.policy_eval import policy_eval
 
 def argmax(d):
     """d (dict)"""
-    max_value = max(d.values)
+    max_value = max(d.values())
     max_key = -1
     for key, value in d.items():
         if value == max_value:
@@ -26,3 +26,45 @@ def greedy_policy(V, env, gamma):
 
     for state in env.states():
         action_values = {}
+
+        for action in env.actions():
+            next_state = env.next_state(state, action)
+            r = env.reward(state, action, next_state)
+            value = r + gamma * V[next_state]
+            action_values[action] = value
+
+        max_action = argmax(action_values)
+        action_probs = {0: 0.0, 1: 0.0, 2: 0.0, 3: 0.0}
+        action_probs[max_action] = 1.0
+        # greedy化した方策を返す
+        pi[state] = action_probs
+    return pi
+
+
+def policy_iter(env, gamma, threshold=0.001, is_render=True):
+    pi = defaultdict(lambda: {0: 0.25, 1: 0.25, 2: 0.25, 3: 0.25})
+    V = defaultdict(lambda: 0)
+
+    while True:
+        # 1. 現状の方策を評価
+        V = policy_eval(pi, V, env, gamma, threshold)
+        # 2. 改善 (greedy化した方策new_piを得る
+        new_pi = greedy_policy(V, env, gamma)
+
+        if is_render:
+            env.render_v(V, pi)
+
+        # 3. 更新確認
+        # もし更新されていなければ、ベルマン最適方程式を満たしているということ。
+        # その時のpi (とnew_pi)が最適方策ということになる
+        if new_pi == pi:
+            break
+        pi = new_pi
+
+    return pi
+
+
+if __name__ == "__main__":
+    env = GridWorld()
+    gamma = 0.9
+    pi = policy_iter(env, gamma)
