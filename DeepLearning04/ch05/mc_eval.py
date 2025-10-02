e7bec5a362d8f93e0577f1b4829a131d83330588
Ryoichi <ryoichikakehi@gmail.com>
2025-10-03 01:40:01 +0900

    modified:   DeepLearning04/ch05/mc_eval.py

--- DeepLearning04/ch05/mc_eval.py
+++ DeepLearning04/ch05/mc_eval.py
@@ -21,3 +21,47 @@ class RandomAgent:
         self.V = defaultdict(lambda: 0)
         self.cnts = defaultdict(lambda: 0)
         self.memory = []
+
+    def get_action(self, state):
+        action_probs = self.pi[state]
+        actions = list(action_probs.keys())
+        probs = list(action_probs.values())
+        return np.random.choice(actions, p=probs)
+
+    def add(self, state, action, reward):
+        data = (state, action, reward)
+        self.memory.append(data)
+
+    def reset(self):
+        self.memory.clear()
+
+    def eval(self):
+        G = 0
+        for data in reversed(self.memory):
+            state, action, reward = data
+            G = self.gamma * G + reward
+            self.cnts[state] += 1
+            self.V[state] += (G - self.V[state]) / self.cnts[state]
+
+
+if __name__ == "__main__":
+    env = GridWorld()
+    agent = RandomAgent()
+
+    episodes = 1000
+    for episode in range(episodes):
+        state = env.reset()
+        agent.reset()
+
+        while True:
+            action = agent.get_action(state)
+            next_state, reward, done = env.step(action)
+
+            agent.add(state, action, reward)
+            if done:
+                agent.eval()
+                break
+
+            state = next_state
+
+    env.render_v(agent.V)
