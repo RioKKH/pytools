3e3c4ff25dce2f7dd8526e0bcb66703510013b65
Ryoichi <ryoichikakehi@gmail.com>
2025-10-08 22:41:37 +0900

    new file:   DeepLearning04/ch06/sarsa.py

---
+++ DeepLearning04/ch06/sarsa.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from collections import defaultdict, deque
+import numpy as np
+from common.gridworld import GridWorld
+from common.utils import greedy_probs
+
+
+class SarsaAgent:
+    def __init__(self):
+        self.gamma = 0.9
+        self.alpha = 0.8
+        self.epsilon = 0.1
+        self.action_size = 4
+
+        random_actions = {0: 0.25, 1: 0.25, 2: 0.25, 3: 0.25}
+        self.pi = defaultdict(lambda: random_actions)
+        self.Q = defaultdict(lambda: 0)
+        self.memory = deque(maxlen=2)
+
+    def get_action(self, state):
+        action_probs = self.pi[state]
+        actions = list(action_probs.keys())
